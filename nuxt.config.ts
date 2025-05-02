37d564eccc2d18a04a641d9cf9c8a2335f6bc94f
Herlambang Kuswicaksonojati <herlambangk25@gmail.com>
2025-05-02 14:48:27 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineNuxtConfig({
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	css: ['./app/assets/css/main.css'],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/fonts',
+		'@nuxt/icon',
+		'@nuxt/image',
+		'@nuxt/content',
+		'@nuxt/test-utils',
+		'@nuxt/ui',
+		'@nuxt/scripts',
+	],
+});
